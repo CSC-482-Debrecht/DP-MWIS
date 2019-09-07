83a13c23211ed9d81521b6add4f126bdc13b8f43
Alex Debrecht <adebrech@ur.rochester.edu>
2019-09-07 16:30:35 -0400

    Added comments

--- DP MWIS Test/test.cpp
+++ DP MWIS Test/test.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 using std::max; using std::cout; using std::min;
 
+// Determine latest possible non-conflicting event for each event l
 void CalcConflicts(Job jobs[], int c[], int size) {
 	c[0] = 0;
 	int endTimes[size];
@@ -31,18 +32,25 @@ void CalcConflicts(Job jobs[], int c[], int size) {
 	}
 }
 
+// Determine value of optimal solution
 int CalcOptimal(Job events[], int c[], const int NUM_EVENTS) {
 	int OPT[NUM_EVENTS + 1][NUM_EVENTS + 1][1<<NUM_EVENTS];
 	memset(OPT, -1, sizeof(OPT));
 	int option1, option2, option3, option4;
+
+	// Base case-ish -- when no one goes to anything, value is 0
+	// Note that if all possible events have already been attended, value is also 0, but this is accounted for on the fly
 	for (int i = 0; i < 1<<NUM_EVENTS; ++i) {
 		OPT[0][0][i] = 0;
 	}
+
+	// Loop over all possible combinations of j, k, and already-chosen events
 	for (int j = 0; j < NUM_EVENTS + 1; ++j) {
 		for (int k = j; k < NUM_EVENTS + 1; ++k) {
 			// mask is k bits long. The n-th bit is unset if event n has been used.
 			for (int mask = (1 << k) - 1; mask >= 0; --mask) {
 				// k > j
+				// In general, can choose neither j nor k, only k, only j, or both
 				if (j > 0) {
 					option1 = OPT[j-1][k-1][mask >> 1];
 					option2 = (OPT[j-1][c[k]][(mask &~ 1) >> min(k-(j-1), k-c[k])] + events[k].getValue())*(mask & 1U); // Check if we've used k yet
@@ -53,17 +61,22 @@ int CalcOptimal(Job events[], int c[], const int NUM_EVENTS) {
 					} else {
 						option4 = 0;
 					}
+				// Easier to split j == 0 case out
 				} else {
 					option1 = 0;
 					option2 = 0;
 					option3 = OPT[0][k-1][mask >> 1];
 					option4 = (OPT[0][c[k]][(mask &~ (1UL << (k-1))) >> (k-c[k])] + events[k].getValue())*(mask & 1U);
 				}
+				// Symmetric across major diagonal
+				// Take largest value
 				OPT[j][k][mask] = OPT[k][j][mask] = max(max(max(option1, option2),option3),option4);
 			}
 		}
 	}
 
+	// Return solution to all events
+	// Easy to refactor to allow any solution to be output
 	return OPT[NUM_EVENTS][NUM_EVENTS][(1 << NUM_EVENTS)-1];
 }
 
@@ -87,15 +100,7 @@ int main() {
 
 	QuickSort<Job>(events, 0, NUM_EVENTS);
 
-	//	for (int i = 0; i < NUM_EVENTS + 1; ++i) {
-	//		cout << "Event: " << i << " Start: " << events[i].getStart() << " End: " << events[i].getEnd() << " Weight: " << events[i].getValue() << "\n";
-	//	}
-
 	CalcConflicts(events, c, NUM_EVENTS + 1);
-
-	//	for (int i = 0; i < NUM_EVENTS + 1; ++i) {
-	//		cout << "First event that does not conflict with event " << i << " is " << c[i] << "\n";
-	//	}
 
 	cout << CalcOptimal(events, c, NUM_EVENTS);
 
